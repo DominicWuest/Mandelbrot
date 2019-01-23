71da5dede199299db91db77fc1283bf0fb778b43
DominicWuest <dominic.wuest@hotmail.com>
2019-01-23 11:32:38 +0100

    Added comments, only zooms relative to the mouses position if it is zooming in now, Cleaned up code a bit

--- Iterator.java
+++ Iterator.java
@@ -1,48 +1,86 @@
-import java.awt.Color;
+
+/*
+ *
+ * This class calculates the pixels inside the mandelbrot set.
+ * It implements Runnable, so that the calculation can happen in parallel
+ *
+*/
 
 class Iterator implements Runnable {
 
+  // Object of the Thread on which the process is going to run on
   Thread myThread;
-  double a, bi;
+  /*
+   *
+   * One Thread caulcates all pixels from the coordinates (0, minY)
+   * to (windowWidth - 1, maxY)
+   * Every thread gets assigned a minY and a maxY so it knows which
+   * pixels it needs to calculate
+   *
+  */
   int minY, maxY;
-  int iterations;
-  double currA, currBi, tempA;
-  int color;
+  // The index of the Thread
   int index;
 
+  // Constructor
   Iterator(int index, int minY, int maxY) {
+
+    // Initializing thread with the name of the given index
     myThread = new Thread(this, Integer.toString(index));
     this.index = index;
     this.minY = minY;
     this.maxY = maxY;
+    // Starting new Thread
     myThread.start();
   }
 
+  // This function runs in parallel and gets called after starting the thread
   public void run() {
+
+    // Iterate over all coordinates expected to calculate
     for (int y = this.minY; y < this.maxY; y++) {
-      bi = map(y, 0, Mandelbrot.windowHeight, Mandelbrot.maxValues[Mandelbrot.TOP], Mandelbrot.maxValues[Mandelbrot.BOTTOM]);
+      // Original imaginary part of the complex number to calculate
+      double bi = map(y, 0, Mandelbrot.windowHeight, Mandelbrot.maxValues[Mandelbrot.TOP], Mandelbrot.maxValues[Mandelbrot.BOTTOM]);
       for (int x = 0; x < Mandelbrot.windowWidth; x++) {
-        this.iterations = 0;
-        a = map(x, 0, Mandelbrot.windowWidth, Mandelbrot.maxValues[Mandelbrot.LEFT], Mandelbrot.maxValues[Mandelbrot.RIGHT]);
-        currA = 0;
-        currBi = 0;
+        // Amount of iterations needed until the function-value was out of bounds (bigger than Mandelbrot.bound)
+        // Gets incremented after every new recursive function call
+        int iterations = 0;
+        // Original real port of the complex number to calculate
+        double a = map(x, 0, Mandelbrot.windowWidth, Mandelbrot.maxValues[Mandelbrot.LEFT], Mandelbrot.maxValues[Mandelbrot.RIGHT]);
+        // Current a (real) and bi (imaginary) part of the complex number to calculate. Used for caluclation of function-value for current (x, y)
+        double currA = 0, currBi = 0, tempA;
+        // Imitating the recursive function call with a for-loop
         for (int i = 0; i <= Mandelbrot.maxIterations; i++) {
           tempA = currA;
           currA = Math.pow(currA, 2) - Math.pow(currBi, 2) + a;
           currBi = 2 * tempA * currBi + bi;
+          // Checks if complex number is out of bounds
           if (Math.sqrt(Math.pow(currA, 2) + Math.pow(currBi, 2)) > Mandelbrot.bound) {
+            // Save the iterations it took for the complex number to be out of bounds (amount of loop-iterations)
             iterations = i;
+            // No need to calculate more, break the loop
             break;
           }
         }
-        color = (int)map(Math.sqrt(map(iterations, 0, Mandelbrot.maxIterations, 0, 1)), 0, 1, 0, 255);
-        Mandelbrot.display[x][y] = color;
+        /*
+         *
+         * Calculating the color (0- 255) by mapping the iterations until the function-value was out of bounds
+         * Which is a value between 0 and maxIterations to a value between 0 and 1
+         * Then mapping the square root of this mapped value to a value between 0 and 255
+         *
+         * Then, assinging the caluclated pixel in the display matrix to the caluclated color
+         *
+        */
+        Mandelbrot.display[x][y] = (int)map(Math.sqrt(map(iterations, 0, Mandelbrot.maxIterations, 0, 1)), 0, 1, 0, 255);
       }
     }
+    // Changes its finished status to true, so to signal, that it has finished calculating
     Mandelbrot.finishedStatus[this.index] = true;
   }
 
-  public static synchronized double map(double a, double firstMin, double firstMax, double secondMin, double secondMax) {
+  // This function maps the parameter a, which is between firstMin and firstMax to a number between secondMin and secondMax
+  // Used to map the complex number for a certain pixel from a coordinate
+  public static double map(double a, double firstMin, double firstMax, double secondMin, double secondMax) {
     return ((a / (firstMax - firstMin)) * (secondMax - secondMin)) + secondMin;
   }
 
--- Mandelbrot.java
+++ Mandelbrot.java
@@ -10,8 +10,8 @@ import static java.lang.Math.signum;
 
 /*
 *
-*  This is a program to easily explore the Mandelbrot Set
-*  It uses multithreading to increase performence and make the navigation smoother
+*  This is a program with the intention of easily exploring the Mandelbrot Set
+*  It uses multithreading to increase performance and make the navigation smoother
 *
 */
 
@@ -29,11 +29,13 @@ class Mandelbrot extends JPanel implements MouseListener, MouseMotionListener, M
   // Variables to hold how wide and tall the displayed window is
   static final int windowWidth = 1000, windowHeight = 1000;
 
-  static Mandelbrot mandelbrot;
+  // Instance of the class itself because of eventlisteners
+  static Mandelbrot mandelbrot = new Mandelbrot();
 
   // Matrix of displayed pixels
   static volatile int[][] display = new int[windowWidth][windowHeight];
 
+  // Main JFrame
   static JFrame frame = new JFrame("Mandelbrot");
 
   // Maximum amount of times to call the Mandelbrot function
@@ -49,9 +51,8 @@ class Mandelbrot extends JPanel implements MouseListener, MouseMotionListener, M
   // Array of the threads' status (true = finished; false = calculating)
   static volatile boolean[] finishedStatus = new boolean[N_THREADS - 1];
 
-  static Long time;
-
-  static BufferedImage canvas;
+  // Where everything gets painted on
+  static BufferedImage canvas = new BufferedImage(windowWidth, windowHeight, BufferedImage.TYPE_INT_ARGB);
 
   // Last x- and y-position of the mouse so that mouseDragged knows how far it should move the picture
   static int lastMouseX, lastMouseY;
@@ -59,12 +60,10 @@ class Mandelbrot extends JPanel implements MouseListener, MouseMotionListener, M
   // Last mouse button which has been pressed (-1 = none, 1 = left-click)
   static int lastButtonPressed = -1;
 
+  // Main function
   public static void main(String[] args) {
 
-    mandelbrot = new Mandelbrot();
-
-    canvas = new BufferedImage(windowWidth, windowHeight, BufferedImage.TYPE_INT_ARGB);
-
+    // Initializing frame and adding eventlisteners to it
     frame.setSize(windowWidth, windowHeight + 30);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
@@ -76,7 +75,6 @@ class Mandelbrot extends JPanel implements MouseListener, MouseMotionListener, M
 
     // Main Loop
     while (true) {
-      time = System.nanoTime();
       // Starts new threads and assigns them the coordinates to calculate
       for (int i = 0; i < iterators.length; i++) {
         iterators[i] = new Iterator(i, windowHeight / (N_THREADS - 1) * i, windowHeight / (N_THREADS - 1) * (i + 1));
@@ -92,28 +90,46 @@ class Mandelbrot extends JPanel implements MouseListener, MouseMotionListener, M
           }
         }
         if (finishedChecker) break;
+        // Repaints after every check to better see progress
         mandelbrot.repaint();
       }
-      //System.out.println((System.nanoTime() - time) / 1000000000.0 + " s");
+      // Repaints after all threads have finished calculating
       mandelbrot.repaint();
     }
   }
 
+  // Function to paint everything needed
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     Graphics2D g2 = (Graphics2D)g;
+    // Draws every pixel of the display matrix
     for (int x = 0; x < windowWidth; x++) {
       for (int y = 0; y < windowHeight; y++) {
+        // setRGB takes the color as a 24-bit integer, Alpha / Red / Green / Blue
         canvas.setRGB(x, y, (255 << 24) | (0 << 16) | (0 << 8) | display[x][y]);
       }
     }
     g2.drawImage(canvas, null, null);
   }
 
+  // Zooms into or out of the image
+  // A negative zoomValue will cause it to zoom out, a positive to zoom in
   public void zoom(double zoomValue, MouseWheelEvent e) {
-    double percentageTop = e.getY() / (double)(windowHeight);
-    double percentageLeft = e.getX() / (double)(windowWidth);
+    // Mouse position in x-axis relative to window in percent
+    double percentageLeft;
+    // Mouse position in y-axis relative to window in percent
+    double percentageTop;
+    // It only zooms realtive to the mouses position if it zooms into the image
+    if (zoomValue > 0) {
+      percentageLeft = e.getX() / (double)(windowWidth);
+      percentageTop = e.getY() / (double)(windowHeight);
+    } else {
+      percentageLeft = 0.5;
+      percentageTop = 0.5;
+    }
+    // Amount to increase max-values in x-axis
     double dx = Math.abs(maxValues[LEFT] - maxValues[RIGHT]) / zoomValue;
+    // Amount to increase max-values in y-axis
     double dy = Math.abs(maxValues[TOP] - maxValues[BOTTOM]) / zoomValue;
     maxValues[RIGHT] -= (1.0 - percentageLeft) * dx;
     maxValues[LEFT] += percentageLeft * dx;
@@ -121,27 +137,43 @@ class Mandelbrot extends JPanel implements MouseListener, MouseMotionListener, M
     maxValues[TOP] += percentageTop * dy;
   }
 
+  // Changes maxIterations (max amount of iterations in iterator-class)
   public void changeMaxIterations(double wheelValue) {
+    // The smaller maxIterations, the more gradually it should de- and increase
+
+    // Changes by 1 if below 10
     if (maxIterations  < 10) maxIterations += signum(wheelValue);
+    // Changes by 2 if below 30
     else if (maxIterations < 30) maxIterations += 2 * signum(wheelValue);
+    // Changes by 10 if above 30
     else maxIterations += 10 * signum(wheelValue);
+    // Unnecessary to have maxIterations below 0, since picture won't change then
     if (maxIterations == -1) maxIterations = 0;
   }
 
+  // Gets called when a mouseButton is pressed and mouse is moved
+  // Navigates over picture
   public void mouseDragged(MouseEvent e) {
+    // Checks if leftmouse button is pressed
     if (lastButtonPressed == 1) {
       // Change in mouse position in x-axis
       int dx = lastMouseX - e.getX();
       // Change in mouse position in y-axis
       int dy = lastMouseY - e.getY();
+      // Amount to increase maxValues in x-axis
       double toIncreaseX = (Math.abs(maxValues[LEFT] - maxValues[RIGHT])) * (dx / (double)windowWidth);
+      // Amount to increase maxValues in y-axis
       double toIncreaseY = (Math.abs(maxValues[TOP] - maxValues[BOTTOM])) * (dy / (double)windowHeight);
+      // Increase maxValues by the above declared values
       maxValues[RIGHT] += toIncreaseX;
       maxValues[LEFT] += toIncreaseX;
       maxValues[BOTTOM] += toIncreaseY;
       maxValues[TOP] += toIncreaseY;
+      // Matrix to hold new image
       int[][] copy = new int[windowWidth][windowHeight];
+      // Iterates over display-matrix to change copy-matrix
       for (int x = 0; x < windowWidth; x++) {
+        // Checks if x + dx is inside array-bounds
         if (x + dx >= 0 && x + dx < windowWidth) {
           for (int y = 0; y < windowHeight; y++) {
             if (y + dy < 0) copy[x][y] = display[x][0];
@@ -155,17 +187,29 @@ class Mandelbrot extends JPanel implements MouseListener, MouseMotionListener, M
           for (int y = 0; y < windowHeight; y++) copy[x][y] = display[index][y];
         }
       }
+      // Assing displayed pixels to newly generated image
       display = copy;
+      // Assings last mouseposition to current one
       lastMouseX = e.getX();
       lastMouseY = e.getY();
     }
   }
 
+/*
+  *
+  *  Gets called when mouse gets moved in any way
+  *  Assings last mouseposition to current one
+  *  Else it would reset to its original position when dragged
+  *  @see Mandelbrot#mouseDragged(MouseEvent e)
+  *
+*/
   public void mouseMoved(MouseEvent e) {
     lastMouseX = e.getX();
     lastMouseY = e.getY();
   }
 
+  // Gets called when scrolling
+  // Zooms if scrolled or changes maxIterations when scrolled while holding ctrl
   public void mouseWheelMoved(MouseWheelEvent e) {
     switch (e.getModifiersEx()) {
       case 0: zoom(1 / e.getPreciseWheelRotation() * -5, e); break;
@@ -173,10 +217,12 @@ class Mandelbrot extends JPanel implements MouseListener, MouseMotionListener, M
     }
   }
 
+  // Gets called when a mousebutton gets pressed, assigning lastMousePressed to that button
   public void mousePressed(MouseEvent e) {
     lastButtonPressed = e.getButton();
   }
 
+  // Gets called when a mousebutton gets released, meaning that no key is pressed
   public void mouseReleased(MouseEvent e) {
     lastButtonPressed = -1;
   }
